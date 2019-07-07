f544d33f097e0c8b1d0a25ad338ba2cb9ae05aed
zhangshujie7 <zhang_shujie7@163.com>
2019-07-07 21:28:24 +0800

    登录（除AJAX验证验证码）都完成，注册页面完成。

---
+++ src/main/java/com/qlu/model/FileModel.java
@@ -0,0 +1,30 @@
+package com.qlu.model;
+
+import com.qlu.entity.File;
+
+/**
+ * @Project: QLU_ClubsMS
+ * @File: FileModel
+ * @Author: 郭雅楠
+ * @Date: 2019/7/6 / 10:28
+ */
+public class FileModel {
+    private File file;
+    private String author;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
